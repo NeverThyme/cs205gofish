a451e9a63cff8deab44ad77151eef58693d5b223
williamsutton7 <39240614+williamsutton7@users.noreply.github.com>
2018-09-29 18:09:33 -0400

    File I/0 and more bug fixes
    smart computer guessing was not working as it should have. I think it is fixed now.

---
+++ FIO.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class FIO {
+    public boolean fWrite(String fName, String in) {
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(fName, true)));
+            out.println(in);
+            out.close();
+            return true;
+        } catch (IOException e) {
+            System.out.println("File write error");
+            return false;
+        }
+    }
+}
